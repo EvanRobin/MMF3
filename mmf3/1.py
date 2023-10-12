66d1b37534f78dcdff4ec32fe8833e4a34f95ccb
EvanRobin <100917489+EvanRobin@users.noreply.github.com>
2023-10-12 18:41:20 +0200

    Add files via upload

--- mmf3/1.py
+++ mmf3/1.py
@@ -1,6 +1,9 @@
 import math
-import numpy as np
 
+file1 = open("1.txt","a")
+file1.truncate(0) #ovo brise
+cols=("x", "+-red(A)", "rekurzija(B)", "1/+red(C)", "clanova")
+file1.write('\t'.join(cols)+'\n')
 def exp_series(x,epsilon):
     k=1
     result = 1.0
@@ -9,7 +12,7 @@ def exp_series(x,epsilon):
         member *= -x/k
         result += member
         k += 1
-    return k,result
+    return k, result
 
 def exp_recursion(x, epsilon): 
     k = 1
@@ -31,6 +34,44 @@ def exp_to_the_power_of(x, epsilon):
         k += 1
     return k, 1/result
 eps=10**(-10)
-for i in range (0,100,10):
-    print(exp_series(i,eps),exp_recursion(i,eps),exp_to_the_power_of(i,eps))
- 
+
+for i in range (0,110,10): 
+    a,b=exp_series(i,eps)
+    c,d=exp_recursion(i,eps)
+    e,f=exp_to_the_power_of(i,eps)
+    cols1=(str(i),str('{:e}'.format(b)),str('{:e}'.format(d)),str('{:e}'.format(f)),str(a))
+    file1.write('\t'.join(cols1)+'\n')
+def exp_series_alt(x, epsilon):
+    k=1
+    result = 1.0
+    member = 1.0
+    while abs(member) > epsilon:
+        member *= -x/k
+        result += member
+        k += 1
+    return k,result
+
+def exp_recursion_alt(x, epsilon): 
+    k = 1
+    result = 1.0
+    member = 1.0
+    while abs(member) > epsilon:
+        member = ((-1)**k)*(x**k)/math.factorial(k)
+        result += member
+        k += 1    
+    return k, result
+
+def  exp_to_the_power_of_alt(x, epsilon): 
+    k = 1
+    member = 1.0
+    result = 1.0
+    while abs(member) > epsilon:
+        member = (x**k)/math.factorial(k)
+        result += member
+        k += 1
+    return k, 1/result
+
+'''exp_series_alt(20,eps)9'''
+
+
+file1.close()
