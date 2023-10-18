f491b3d728b51413a95a8cfa125857214754ba41
EvanRobin <100917489+EvanRobin@users.noreply.github.com>
2023-10-18 21:10:55 +0200

    Add files via upload

--- mmf3/2.py
+++ mmf3/2.py
@@ -22,3 +22,31 @@ for i in range (0,11):
         line.append('{:e}'.format(d2fdx2_e_to_the_x(i,h)))
         line.append('{:e}'.format(error(d2fdx2_e_to_the_x(i,h),math.exp(i))))
     file1.write('\t'.join(str(item) for item in line) + '\n')
+
+filex1 = open("2x1.txt","a")
+filex1.truncate(0)
+cols=("h", "error")
+filex1.write('\t'.join(cols)+'\n')
+for l in range (1,7):
+    h=10**(-l)
+    cols=(str(h),str('{:e}'.format(error(d2fdx2_e_to_the_x(1,h),math.exp(1)))))
+    filex1.write('\t'.join(cols) + '\n')
+
+filex5 = open("2x5.txt","a")
+filex5.truncate(0)
+cols=("h", "error")
+filex5.write('\t'.join(cols)+'\n')
+for l in range (1,7):
+    h=10**(-l)
+    cols=(str(h),str('{:e}'.format(error(d2fdx2_e_to_the_x(5,h),math.exp(5)))))
+    filex5.write('\t'.join(cols) + '\n')
+
+filex10 = open("2x10.txt","a")
+filex10.truncate(0)
+cols=("h", "error")
+filex10.write('\t'.join(cols)+'\n')
+for l in range (1,7):
+    h=10**(-l)
+    cols=(str(h),str('{:e}'.format(error(d2fdx2_e_to_the_x(10,h),math.exp(10)))))
+    filex10.write('\t'.join(cols) + '\n')
+        
